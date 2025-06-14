af381c7d81375bc25f3a12a9231316690b55205b
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-06-14 13:38:40 +0530

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoList from './components/TodoList'
 const App = () => {
   return (
     <>
-      <div className='h-full md:h-screen p-4 md:p-16 text-gray-800 flex flex-col-reverse md:flex-row items-center justify-center gap-4 md:gap-8'>
+      <div className='h-full md:h-screen p-6 bg-amber-700 md:p-16 text-gray-800 flex flex-col-reverse md:flex-row items-center justify-center gap-4 md:gap-8'>
         <TodoList /> 
         <AddToDo />
       </div>
